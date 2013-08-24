db8469ca65ff175cbfa70971640d4119fd151807
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2013-08-23 17:42:25 -0700

    Cleaning and final bugfixes.

--- macswig.sh
+++
@@ -1,3 +0,0 @@
-swig -v -python linac.i
-gcc -fPIC -c linac_param.c filter.c linac_wrap.c -I/usr/include/python2.7 -I/usr/include/numpy
-ld -dynamiclib linac_param.o filter.o linac_wrap.o -o _linac.so
