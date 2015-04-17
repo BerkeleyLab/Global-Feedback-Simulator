2c1b2d78fbe666e23c61b319fea02a5947a4e686
Carlos Serrano <CSerrano@lbl.gov>
2015-04-16 18:24:10 -0700

    Removed commented line from Cryomodule test

--- source/cryomodule_test.py
+++ source/cryomodule_test.py
@@ -72,7 +72,6 @@ def unit_Cryomodule():
 
     Tmax = 0.05
     
-    # test_file = "source/configfiles/unit_tests/cavity_test_step1.json"
     test_file = "source/configfiles/unit_tests/cryomodule_test.json"
 
     run_Cryomodule_test(Tmax, test_file)
